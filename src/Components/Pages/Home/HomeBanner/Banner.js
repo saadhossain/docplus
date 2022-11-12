175caa6ca4666952d5dfb195cdfcef66107855d8
saadhossain <mshsarkar@gmail.com>
2022-11-12 19:47:59 +0600

    Home Banner added

---
+++ src/Components/Pages/Home/HomeBanner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import chair from '../../../../assests/img/chair.png'
+const Banner = () => {
+    return (
+        <div className='w-11/12 mx-auto flex items-center gap-10 py-5'>
+            <div className='w-full lg:w-2/4'>
+                <h1 className='text-5xl font-bold'>Your New <br/>Smile Starts Here</h1>
+                <p className='text-lg my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis soluta cupiditate, labore quisquam alias, voluptates aspernatur recusandae dolore ut quos atque molestias sequi necessitatibus eaque dolores quasi optio sunt rem.</p>
+                <button className='doc-btn'>Book Now</button>
+            </div>
+            <div className='w-full lg:w-2/4'>
+                <img src={chair} alt='Hospital Chair'/>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
